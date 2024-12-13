f1dfbfe57bb06413842b121b942bd5758864bf3a
Ilia4 <118041365+Ilia4@users.noreply.github.com>
2024-12-13 13:47:46 +0300

    Add files via upload

---
+++ расходы/расходы/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System.Windows.Forms;
+
+namespace расходы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.category = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputSum = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd.MM.yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 48);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // category
+            // 
+            this.category.FormattingEnabled = true;
+            this.category.Items.AddRange(new object[] {
+            "Супермаркет",
+            "Развлечения",
+            "Аптека",
+            "Такси"});
+            this.category.Location = new System.Drawing.Point(12, 75);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(200, 21);
+            this.category.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputSum
+            // 
+            this.inputSum.Location = new System.Drawing.Point(13, 13);
+            this.inputSum.Name = "inputSum";
+            this.inputSum.Size = new System.Drawing.Size(199, 20);
+            this.inputSum.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 236);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(430, 176);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(367, 389);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 6;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 450);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.inputSum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Учёт расходов";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox category;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputSum;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Button DeleteButton;
+    }
+}
+
---
+++ расходы/расходы/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace расходы
+{
+    public partial class Form1 : Form
+    {
+        private List<Expense> ExpenseList = new List<Expense>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Expense
+        {
+            public string Category { get; set; }
+            public decimal Amount { get; set; }
+            public DateTime Date { get; set; }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sum = inputSum.Text + ",00";
+
+            if (string.IsNullOrEmpty(sum))
+            {
+                MessageBox.Show("Заполните строку.", "Пустая строка!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (decimal.TryParse(sum, out decimal amount))
+            {
+                var categoryValue = category.SelectedItem;
+                if (categoryValue == null)
+                {
+                    MessageBox.Show("Выберите категорию!", "Пустое поле", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+
+
+                    if (amount > 0)
+                    {
+
+                        dateTimePicker1.CustomFormat = "dd.MM.yyy";
+
+                        var task = new Expense
+                        {
+                            Category = categoryValue.ToString(),
+                            Amount = amount,
+                            Date = dateTimePicker1.Value,
+                        };
+
+                        ExpenseList.Add(task);
+                        UpdateData();
+                        ResetFields();
+                    }
+                    else
+                    {
+                        // Если сумма меньше или равна 0
+                        MessageBox.Show("Введите число больше 0.", "Неверный формат", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            else
+            {
+                // Если не удалось преобразовать в decimal
+                MessageBox.Show("Введите корректное число.", "Неверный формат", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void ResetFields()
+        {
+            inputSum.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            category.SelectedIndex = -1;
+            MessageBox.Show("Добавлена новая запись.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void CustomizeGridView()
+        {
+            if (dataGridView1.Columns["Category"] != null)
+                dataGridView1.Columns["Category"].HeaderText = "Категория";
+
+            if (dataGridView1.Columns["Amount"] != null)
+                dataGridView1.Columns["Amount"].HeaderText = "Сумма";
+
+            if (dataGridView1.Columns["Date"] != null)
+                dataGridView1.Columns["Date"].HeaderText = "Дата";
+            dataGridView1.Columns["Date"].DefaultCellStyle.Format = "dd.MM.yyyy";
+        }
+
+        private void UpdateData()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = ExpenseList;
+            CustomizeGridView();
+        }
+
+        private void inputSum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Разрешаем ввод только цифр, точку и Backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '.' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            // Запрещаем ввод более одной точки
+            if (e.KeyChar == '.' && (sender as TextBox).Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void DeleteRows()
+        {
+            if (dataGridView1.Rows.Count == 0) 
+            { 
+                MessageBox.Show("Выберите хотя бы одну строку для удаления!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                foreach (DataGridViewRow selectedRow in dataGridView1.SelectedRows)
+                {
+                    int rowIndex = selectedRow.Index;
+
+                    if (rowIndex >= 0 && rowIndex < ExpenseList.Count)
+                    {
+                        ExpenseList.RemoveAt(rowIndex);
+                        UpdateData();
+                    }
+                }
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            DeleteRows();
+        }
+    }
+}
